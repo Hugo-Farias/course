4ad934230cdb505eb89afd201cdf1adb172c40e4
Hugo-Farias <hg80rod@gmail.com>
2024-06-05 02:42:50 -0300

    vitest setup

--- src/component/Course.tsx
+++ src/component/Course.tsx
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import { motion } from "framer-motion";
 import "./Course.scss";
 
-const Course: React.FC = () => {
+const Course = function () {
   const [count, setCount] = useState(0);
 
   return (
     <div className="counter-container">
-      <h1>Counter: {count}</h1>
+      <h1>
+        Counter:
+        <motion.span
+          key={count}
+          initial={{ opacity: 0, y: -10 }} // Initial state
+          animate={{ opacity: 1, y: 0 }} // Animated state
+          transition={{ duration: 0.1 }} // Transition configuration
+        >
+          {count}
+        </motion.span>
+      </h1>
       <div className="button-group">
-        <button onClick={() => setCount(count + 1)}>Increase</button>
+        <button onClick={() => setCount((prev) => prev - 1)}>Decrease</button>
         <button onClick={() => setCount(0)}>Reset</button>
-        <button onClick={() => setCount(count - 1)}>Decrease</button>
+        <button onClick={() => setCount((prev) => prev + 1)}>Increase</button>
       </div>
     </div>
   );
